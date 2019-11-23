c8436ecace05b42f9a28ffeb6483ebfc205566f7
Abtisam Ahmed Ansari <abtisamahmed92@gmail.com>
2019-11-23 14:56:22 +0500

    Updates

---
+++ precache-manifest.d6d9da70dbbcfdebee530ac73b480ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2832f6ef527f0098f23082156f95e75",
+    "url": "/image-gallery-offline/index.html"
+  },
+  {
+    "revision": "ab09c534870253b65142",
+    "url": "/image-gallery-offline/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "fae24373b05fa704d0e3",
+    "url": "/image-gallery-offline/static/css/main.52232234.chunk.css"
+  },
+  {
+    "revision": "ab09c534870253b65142",
+    "url": "/image-gallery-offline/static/js/2.00c4f623.chunk.js"
+  },
+  {
+    "revision": "fae24373b05fa704d0e3",
+    "url": "/image-gallery-offline/static/js/main.6ff148ee.chunk.js"
+  },
+  {
+    "revision": "33effa446d8dd05a40f1",
+    "url": "/image-gallery-offline/static/js/runtime-main.1277bc71.js"
+  }
+]);
